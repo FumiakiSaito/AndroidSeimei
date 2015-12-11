886c7d3a1e2c355922106a43d22078cb4793c140
fumiaki <haratairani3000points@gmail.com>
2015-12-11 15:51:11 +0900

    入力テキストが空文字だった場合、エラーを表示するように

--- AndroidSeimeiApp/app/src/main/java/io/fumiaki/androidseimeiapp/MyForm.java
+++ AndroidSeimeiApp/app/src/main/java/io/fumiaki/androidseimeiapp/MyForm.java
@@ -1,13 +1,10 @@
 package io.fumiaki.androidseimeiapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyForm extends AppCompatActivity {
 
@@ -17,36 +14,40 @@ public class MyForm extends AppCompatActivity {
         setContentView(R.layout.activity_my_form);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my_form, menu);
-        return true;
-    }
+    public void getScore(View view) {
+        // EditText取得
+        EditText myEditText = (EditText) findViewById(R.id.myEditText);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // EditTextの中身を取得
+        String myName = myEditText.getText().toString().trim();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (myName.equals("")) {
+
+            myEditText.setError("please enter your name");
+
+            /*
+            Toast.makeText(
+                    this,
+                    "please enter your name",
+                    Toast.LENGTH_LONG
+            ).show();
+            */
+
+            /*
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder
+                    .setTitle("Error")
+                    .setMessage("please enter your name")
+                    .setPositiveButton("OK", null);
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            */
+
+        } else {
+            // 次の画面へ
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
